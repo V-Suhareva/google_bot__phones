2702de96fdea368d9101b50946fac9c86a1b38d8
V-Suhareva <34304325+V-Suhareva@users.noreply.github.com>
2017-12-06 14:26:49 +0300

    google_bot
    google open added

---
+++ google_bot/google_bot/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.PhantomJS;
+
+namespace google_bot
+{
+    public partial class Form1 : Form
+    {
+        IWebDriver PJS;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PJS = new PhantomJSDriver();
+            PJS.Manage().Window.Maximize();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            PJS.Quit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            PJS.Navigate().GoToUrl("https://www.google.by");
+            (PJS as PhantomJSDriver).GetScreenshot().SaveAsFile("d:\\google_bot\\test.jpg", OpenQA.Selenium.ScreenshotImageFormat.Jpeg);
+        }
+    }
+}
